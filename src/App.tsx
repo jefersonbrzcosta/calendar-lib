df647c49ffb65503453556e9b4fbb04fbd575f61
Jeferson Costa <jefersonbrz.costa@gmail.com>
2024-08-24 11:50:48 -0300

    feat(): finished all views

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,68 @@
+import { useState } from 'react';
 import { CalendarProvider } from './context/CalendarContext';
-import CalendarView from './components/CalendarView/CalendarView';
+import CalendarView from './components/CalendarView/MontlyView';
+import WeeklyView from './components/CalendarView/WeeklyView';
+import DayView from './components/CalendarView/DayView';
+import { Tabs } from 'antd';
+import { CalendarOutlined, BarsOutlined, ClockCircleOutlined } from '@ant-design/icons';
 
+const { TabPane } = Tabs;
 
 function App() {
+  const [activeView, setActiveView] = useState<string>('day');
+
+  const handleTabChange = (key: string) => {
+    setActiveView(key);
+  };
+
   return (
     <CalendarProvider>
-        <div className="w-full flex flex-col">
-          <div className=" p-4 bg-white shadow-lg rounded-lg">
-            <CalendarView />
-          </div>
+      <div className="w-full flex flex-col">
+        <div className="p-4 bg-white shadow-lg rounded-lg">
+          {/* View Selector */}
+          <Tabs
+            defaultActiveKey="day"
+            activeKey={activeView}
+            onChange={handleTabChange}
+            size="large"
+            tabBarGutter={16}
+            tabBarStyle={{ marginBottom: '1rem' }}
+          >
+            <TabPane
+              tab={
+                <span>
+                  <CalendarOutlined />
+                  {`  Monthly View`}
+                </span>
+              }
+              key="month"
+            />
+            <TabPane
+              tab={
+                <span>
+                  <BarsOutlined />
+                  {`  Weekly View`}
+                </span>
+              }
+              key="week"
+            />
+            <TabPane
+              tab={
+                <span>
+                  <ClockCircleOutlined />
+                  {`  Daily View`}
+                </span>
+              }
+              key="day"
+            />
+          </Tabs>
+
+          {/* Render the selected view */}
+          {activeView === 'month' && <CalendarView />}
+          {activeView === 'week' && <WeeklyView />}
+          {activeView === 'day' && <DayView />}
         </div>
+      </div>
     </CalendarProvider>
   );
 }
